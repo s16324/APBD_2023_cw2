f00df57f46e8df8e64435f461026ab5071e97596
s16324 <s16324@pjwstk.edu.pl>
2022-02-06 20:18:32 +0100

    HW

---
+++ cwiczenia-2-s16324/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cwiczenia_2_s16324
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
